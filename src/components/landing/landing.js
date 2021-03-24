a59672dd11027a82be4a1124167a4c73501b7f03
jessy <jessynnebs@gmail.com>
2021-03-24 16:02:16 +0100

    chakra ui working

---
+++ src/components/landing/landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    extendTheme,
+  } from '@chakra-ui/react';
+
+
+
+
+function landing() {
+    return (
+        <div>
+             <Box w="100%" h="200px" bg="blue.900"/>
+           
+        </div>
+    )
+}
+
+export default landing
